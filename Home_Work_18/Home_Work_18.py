5fa0ce65d1b5c80046521373a743feb9bec5345b
pm72 <46081859+pm72@users.noreply.github.com>
2024-02-08 22:57:57 +0400

    pm72

--- Home_Work_18/Home_Work_18.py
+++ Home_Work_18/Home_Work_18.py
@@ -10,27 +10,27 @@ print(v3)  # Output: (5, 7)
 
 """
 
-# class Vector:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-#     def __add__(self, other):
-#         if isinstance(other, Vector):
-#             return Vector(self.x + other.x, self.y + other.y)
-#         else:
-#             raise TypeError("Unsupported operation in vectors")
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
+        else:
+            raise TypeError("Unsupported operation in vectors")
         
-#     def __str__(self):
-#         return f"({self.x}, {self.y})"
+    def __str__(self):
+        return f"({self.x}, {self.y})"
         
         
 
 
-# v1 = Vector(4, 3)
-# v2 = Vector(3, 0)
-# v3 = v1 + v2
-# print(v3)
+v1 = Vector(4, 3)
+v2 = Vector(3, 0)
+v3 = v1 + v2
+print(v3)
 
 
 """
@@ -47,21 +47,21 @@ print(book1 == book3)  # Output: False
 
 """
 
-# class Book:
-#     def __init__(self, title, author):
-#         self.title = title
-#         self.author = author
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
 
     
-#     def __eq__(self, other):
-#         if isinstance(other, Book):
-#             return self.title == other.title and self.author == other.author
-#         return False
+    def __eq__(self, other):
+        if isinstance(other, Book):
+            return self.title == other.title and self.author == other.author
+        return False
 
 
-# book1 = Book('1984', 'George Orwell')
-# book2 = Book('1984', 'George Orwell')
-# book3 = Book('Brave New World', 'Aldous Huxley')
-# print(book1 == book2)  # Output: True
-# print(book1 == book3)  # Output: False
+book1 = Book('1984', 'George Orwell')
+book2 = Book('1984', 'George Orwell')
+book3 = Book('Brave New World', 'Aldous Huxley')
+print(book1 == book2)  # Output: True
+print(book1 == book3)  # Output: False
         
